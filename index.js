40af711cd345460ac6232c54245447bef4111380
shazninIshrat <shaznin.ishrat@gmail.com>
2021-08-02 18:27:05 +0600

    index added

---
+++ index.js
@@ -0,0 +1,26 @@
+// Task-2
+var firstBookPrice = 100;
+var secondBookPrice = 100.50;
+var bookName = "pother pachali";
+var isPriceHigh = true;
+console.log(firstBookPrice);
+console.log(secondBookPrice);
+console.log(bookName);
+console.log(isPriceHigh);
+
+// Task-3
+var productPrice = '200';
+var productPrice1 = 100;
+var newProductPrice = parseInt(productPrice);
+var totalPrice = (newProductPrice + productPrice1);
+
+
+
+console.log(parseInt(totalPrice));
+
+// Task-4
+var upperBoundary = 20;
+var lowerBoundary = 10;
+var randomNumber = Math.round(Math.random() * (upperBoundary - lowerBoundary + 1)) + lowerBoundary;
+console.log(randomNumber);
+
